fac1093fed809bea50e6391dc6ee1af0dc2d3598
Anton Vitalevich <kulchickii-antoh@mail.ru>
2025-04-08 22:08:26 +0300

    update keyboard

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,13 +1,19 @@
 import { useEffect } from 'react';
 import styles from './Keyboard.module.css'
 
-const LETTERS = 'qwertyuiopasdfghjklzxcvbnm'
+// const LETTERS = 'qwertyuiopasdfghjklzxcvbnm'
+
+const ROWS = [
+  'qwertyuiop',
+  'asdfghjkl',
+  'zxcvbnm'
+]
 
 export const Keyboard = ({ handleBackspace, handleSubmitWord, handleLetterInput, onKeyboardCheckLetters, disabled }) => {
   useEffect(() => {
     const handleKeyDown = (e) => {
       const key = e.key.toLowerCase()
-      if (LETTERS.includes(key)) {
+      if (ROWS.join('').includes(key)) {
         handleLetterInput(key)
       }
       else if (key === 'enter') {
@@ -23,20 +29,42 @@ export const Keyboard = ({ handleBackspace, handleSubmitWord, handleLetterInput,
   }, []);
   
   return (
-    <div className={styles.keyboard_module}>
-      {LETTERS.split('').map(el => {
-        const check = Object.hasOwn(onKeyboardCheckLetters, el) 
-        return <button
-          disabled={disabled}
-          key={el}
-          onClick={() => handleLetterInput(el)}
-          className={`${check ? styles[onKeyboardCheckLetters[el]] : ''}`}
-        >
-          {el.toUpperCase()}
-        </button>
-      })}
-      <button disabled={disabled} className={`${styles.backspace}`} onClick={() => handleBackspace()}>back</button>
-      <button disabled={disabled} className={`${styles.enter}`} onClick={() => handleSubmitWord()}>enter</button>
+    <div className={styles.keyboard}>
+      {ROWS.map((row, rowIndex) => (
+        <div key={rowIndex} className={styles.row}>
+          {rowIndex === 2 && 
+            <button
+              disabled={disabled}
+              onClick={handleSubmitWord}
+              className={`${styles.key} ${styles.enter}`}
+            >
+              enter
+            </button>
+          }
+          {row.split('').map((letter) => {
+            const check = Object.hasOwn(onKeyboardCheckLetters, letter);
+            return (
+              <button
+                disabled={disabled}
+                key={letter}
+                onClick={() => handleLetterInput(letter)}
+                className={`${styles.key} ${check ? styles[onKeyboardCheckLetters[letter]] : ''}`}
+              >
+                {letter.toUpperCase()}
+              </button>
+            )
+          })}
+          {rowIndex === 2 && 
+            <button
+              disabled={disabled}
+              onClick={handleBackspace}
+              className={`${styles.key} ${styles.backspace}`}
+            >
+            back
+            </button>
+          }
+        </div>
+      ))}
     </div>
-  );
+    )
 }
